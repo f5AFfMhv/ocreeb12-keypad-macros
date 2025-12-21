8d6b482a3782feaa83420911084d536e83833b03
Martynas <mjankunas@gmail.com>
2025-12-21 19:25:40 +0200

    enable hide drive upon start

--- boot.py
+++ boot.py
@@ -1,21 +1,21 @@
-# import supervisor
-# import board
-# import storage
-# import usb_cdc
+import supervisor
+import board
+import storage
+import usb_cdc
 
-# from digitalio import DigitalInOut, Direction, Pull
+from digitalio import DigitalInOut, Direction, Pull
 
-# supervisor.set_next_stack_limit(4096 + 4096)
+supervisor.set_next_stack_limit(4096 + 4096)
 
-# row = DigitalInOut(board.D7)
-# col = DigitalInOut(board.D6)
+row = DigitalInOut(board.D7)
+col = DigitalInOut(board.D6)
 
-# row.direction = Direction.INPUT
-# col.direction = Direction.OUTPUT
+row.direction = Direction.INPUT
+col.direction = Direction.OUTPUT
 
-# row.pull = Pull.DOWN
-# col.value = True
+row.pull = Pull.DOWN
+col.value = True
 
-# if not row.value:
-#     storage.disable_usb_drive()
-#     usb_cdc.disable()
+if not row.value:
+    storage.disable_usb_drive()
+    usb_cdc.disable()
